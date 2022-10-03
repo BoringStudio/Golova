bb96cd37ec4e0376c9408afbca2ccc64b33fe7c6
Ivan Kalinin <i.kalinin@dexpa.io>
2022-10-03 23:45:29 +0200

    Nearly finish

--- game.gd
+++ game.gd
@@ -13,6 +13,8 @@ const LEVELS = [
 		solution = "Living things",
 		variants = [{
 			sequence = [Item.Chicken, Item.Elephant, Item.Octopus, Item.Clown, Item.Cow],
+			exclude = [Item.Cat, Item.Crab],
+			hint = null,
 			rows = 4,
 			cols = 4,
 			empty_cells = 0
@@ -22,18 +24,22 @@ const LEVELS = [
 		solution = "Things you can theoretically fit in your mouth",
 		variants = [{
 			sequence = [Item.Cockroach, Item.Dice, Item.Lipstick, Item.Money, Item.Eye],
-			hint = "You can't eat what I can. I can eat what you can",
+			exclude = [Item.Cat, Item.Crab, Item.Lamp, Item.Domino, Item.Paper, Item.Key, Item.Pill, Item.Lighter, Item.Compass, Item.Egg, Item.ChessPiece, Item.Nose, Item.Ladybug, Item.Strawberry, Item.AceOfSpades],
+			hint = "You can't eat what I can.\nI can eat what you can.",
 			rows = 5,
 			cols = 5,
 			empty_cells = 4,
 		}, {
 			sequence = [Item.Pill, Item.Lighter, Item.Compass, Item.Egg, Item.ChessPiece],
-			hint = "Big desires and small mouths",
+			exclude = [Item.Cat, Item.Crab, Item.Lamp, Item.Domino, Item.Paper, Item.Key, Item.Cockroach, Item.Dice, Item.Lipstick, Item.Money, Item.Eye, Item.Nose, Item.Ladybug, Item.Strawberry, Item.AceOfSpades],
+			hint = "Big desires and small mouths.",
 			rows = 5,
 			cols = 5,
 			empty_cells = 4,
 		}, {
 			sequence = [Item.Nose, Item.Ladybug, Item.Strawberry, Item.AceOfSpades],
+			exclude = [Item.Cat, Item.Crab, Item.Lamp, Item.Domino, Item.Paper, Item.Key, Item.Cockroach, Item.Dice, Item.Lipstick, Item.Money, Item.Eye, Item.Pill, Item.Lighter, Item.Compass, Item.Egg, Item.ChessPiece],
+			hint = null,
 			rows = 5,
 			cols = 5,
 			empty_cells = 4,
@@ -43,18 +49,22 @@ const LEVELS = [
 		solution = "Things you can open",
 		variants = [{
 			sequence = [Item.Eye, Item.Microwave, Item.Ambrella, Item.Chest, Item.BeerBottle, Item.Car],
-			hint = "You can't get out until you touch the knob",
+			exclude = [Item.Cat, Item.Crab, Item.Lock, Item.CashMachine, Item.Helicopter, Item.WindMill, Item.Mouth, Item.Elevator, Item.Camera, Item.Safe, Item.Bag, Item.Refrigirator, Item.Door, Item.Prezent, Item.Car, Item.WashingMachine],
+			hint = "You can't get out until you touch the knob.",
 			rows = 5,
 			cols = 5,
 			empty_cells = 4,
 		}, {
 			sequence = [Item.Mouth, Item.Elevator, Item.Camera, Item.Safe, Item.Bag, Item.Refrigirator],
-			hint = "Don't close all the doors behind you. Sometimes you have to come back",
+			exclude = [Item.Cat, Item.Crab, Item.Lock, Item.CashMachine, Item.Helicopter, Item.WindMill, Item.Eye, Item.Microwave, Item.Ambrella, Item.Chest, Item.BeerBottle, Item.Car, Item.Door, Item.Prezent, Item.Car, Item.WashingMachine],
+			hint = "Don't close all the doors behind you.\nSometimes you have to come back.",
 			rows = 5,
 			cols = 5,
 			empty_cells = 4,
 		}, {
 			sequence = [Item.Door, Item.Prezent, Item.Car, Item.WashingMachine],
+			exclude = [Item.Cat, Item.Crab, Item.Lock, Item.CashMachine, Item.Helicopter, Item.WindMill, Item.Mouth, Item.Elevator, Item.Camera, Item.Safe, Item.Bag, Item.Refrigirator, Item.Eye, Item.Microwave, Item.Ambrella, Item.Chest, Item.BeerBottle, Item.Car],
+			hint = null,
 			rows = 5,
 			cols = 5,
 			empty_cells = 4,
@@ -64,18 +74,22 @@ const LEVELS = [
 		solution = "Rotating things",
 		variants = [{
 			sequence = [Item.MerryGoRound, Item.Planet, Item.Bike, Item.Clock, Item.WashingMachine, Item.Revolver],
-			hint = "I ate Jupiter for breakfast. And now my stomach is twisting",
+			exclude = [Item.Cat, Item.Crab, Item.Lighter, Item.OfficeChair, Item.Lipstick, Item.Cannon, Item.Lighthouse, Item.Helicopter, Item.Drill, Item.Compass, Item.CorkScrew, Item.Wheel, Item.SpinningTop, Item.WheelChair, Item.WindMill, Item.ToiletPaper],
+			hint = "I ate Jupiter for breakfast.\nAnd now my stomach is twisting.",
 			rows = 6,
 			cols = 6,
 			empty_cells = 4,
 		}, {
 			sequence = [Item.Lighthouse, Item.Helicopter, Item.Drill, Item.Compass, Item.CorkScrew, Item.Wheel],
-			hint = "Don't look too long before you eat it. It can be hypnotizing",
+			exclude = [Item.Cat, Item.Crab, Item.Lighter, Item.OfficeChair, Item.Lipstick, Item.Cannon, Item.MerryGoRound, Item.Planet, Item.Bike, Item.Clock, Item.WashingMachine, Item.Revolver, Item.SpinningTop, Item.WheelChair, Item.WindMill, Item.ToiletPaper],
+			hint = "Don't look too long before you eat it.\nIt can be hypnotizing.",
 			rows = 6,
 			cols = 6,
 			empty_cells = 4,
 		}, {
 			sequence = [Item.SpinningTop, Item.WheelChair, Item.WindMill, Item.ToiletPaper],
+			exclude = [Item.Cat, Item.Crab, Item.Lighter, Item.OfficeChair, Item.Lipstick, Item.Cannon, Item.MerryGoRound, Item.Planet, Item.Bike, Item.Clock, Item.WashingMachine, Item.Revolver, Item.Lighthouse, Item.Helicopter, Item.Drill, Item.Compass, Item.CorkScrew, Item.Wheel],
+			hint = null,
 			rows = 6,
 			cols = 6,
 			empty_cells = 4,
@@ -89,9 +103,11 @@ onready var _items = $Items
 onready var _camera = $Camera
 onready var _camera_target = $CameraTarget
 onready var _music = $Music
+onready var _idle_music = $IdleMusic
 onready var _main_menu = $CanvasLayer/MainMenu
 onready var _pause_menu = $CanvasLayer/PauseMenu
 onready var _animation: AnimationPlayer = $AnimationPlayer
+onready var _hint: Label3D = $StaticBody/Hint
 
 export(float) var item_width = 1.0
 export(float) var item_height = 1.0
@@ -100,8 +116,8 @@ export(int) var cols = 4
 export(int) var empty_cell_count = 4
 export(Array, Item) var sequence = []
 
-var _current_level = 0
-var _current_variant = 0
+var _current_level = 3
+var _current_variant = 2
 var _ui_state = UiState.Menu
 var _eaten_cell: Cell = null
 var _last_marked_cell: Cell = null
@@ -131,7 +147,7 @@ func _on_game_started():
 
 
 func _on_animation_finished(anim):
-	if anim == "fade_in":
+	if anim == "fade_in" or anim == "show_hint" or anim == "win":
 		_head_timer.start()
 		_regenerate()
 
@@ -200,6 +216,8 @@ func _regenerate():
 	rows = variant.rows
 	cols = variant.cols
 	empty_cell_count = variant.empty_cells
+	var exclude = variant.exclude
+	_hint.text = variant.hint if variant.hint != null else ""
 
 	if rows == 0 or cols == 0:
 		return
@@ -215,7 +233,7 @@ func _regenerate():
 
 	var other_cells: Array = []
 	for i in range(Item.MAX):
-		if sequence.find(i) < 0:
+		if sequence.find(i) < 0 and exclude.find(i) < 0:
 			other_cells.append(i)
 
 	randomize()
@@ -247,7 +265,8 @@ func _on_time_to_eat():
 		return
 
 	if not _music.playing:
-		_music.play();
+		_idle_music.stop()
+		_music.play()
 
 	var next_seq_idx = 0
 	var cell_to_eat = null
@@ -282,21 +301,40 @@ func _on_finished_eating(damage: int):
 	_eaten_cell = null
 	if sequence.empty():
 		_focus_eyes(_camera)
-		#_head.make_angry()
+		_head.make_angry()
 
 		var current_level = LEVELS[_current_level]
+		var variant = current_level.variants[_current_variant]
+
+		var victory = damage >= 3
+		if victory:
+			_current_variant = 1000
+
 		if _current_variant + 1 < current_level.variants.size():
 			_current_variant += 1
 		elif _current_level + 1 < LEVELS.size():
 			_current_level += 1
 			_current_variant = 0
 		else:
-			pass
+			_clear_field()
+			_animation.play("final")
+			return
 
-		_head._set_damage(0)
-		_regenerate()
+		_clear_field()
 
-	_head_timer.start()
+		if variant.hint != null and not victory:
+			_head._set_damage(0)
+			_animation.play("show_hint")
+		else:
+			var timer = get_tree().create_timer(2.5)
+			timer.connect("timeout", self, "_on_restore_health")
+			_animation.play("win")
+	else:
+		_head_timer.start()
+
+
+func _on_restore_health():
+	_head._set_damage(0)
 
 
 func _focus_eyes(target: Spatial):
